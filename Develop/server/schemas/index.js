a1358fe28723d52c097b24820b69f400fd10ca00
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-10-13 21:42:19 +1100

    created schemas folder, added code for index.js

--- Develop/server/schemas/index.js
+++ Develop/server/schemas/index.js
@@ -1,10 +1,13 @@
+// Importing dependencies
 const { makeExecutableSchema } = require('graphql-tools');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
+// Creating GraphQL schema by passing 2 objects, typedefs and resolvers
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
+// Exporting schema
 module.exports = schema;
