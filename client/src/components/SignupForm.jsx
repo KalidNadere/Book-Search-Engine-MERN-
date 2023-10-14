fd31c450380170e87451af226119cf4356e725bb
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-10-14 20:52:26 +1100

    updated Signup form to replace addUser with ADD_USER mutation

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client'; // Import useMutation from Apollo Client
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from '../utils/mutations'; // Import the ADD_USER mutation
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  // Use useMutation to execute the ADD_USER mutation
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,20 +32,18 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // Use the addUser mutation to create a new user
+      const { data } = await addUser({
+        variables: userFormData,
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { token, user } = data.addUser;
+        Auth.login(token);
+      } catch (err) {
+        console.error(err);
+        setShowAlert(true);
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
+    
     setUserFormData({
       username: '',
       email: '',
